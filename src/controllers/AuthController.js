65f299ec83f0d97e7aa1372f24099a77e65a773b
ChovyDoinb <binhdinhqt137@gmail.com>
2021-11-11 10:34:27 +0700

    fiz: lowwercase model name

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 class AuthControler {
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 
 class UserController {
   updateUser = async (req, res) => {
