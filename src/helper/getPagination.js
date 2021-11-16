79b73900efa284dbe24d5fef8e24ea621b20a5c6
ChovyDoinb <binhdinhqt137@gmail.com>
2021-11-17 00:59:44 +0700

    Fix: pageIndex

--- src/helper/getPagination.js
+++ src/helper/getPagination.js
@@ -1,6 +1,7 @@
 //offset: quantity of items to skip
 //limit: quantity of items to fetch
-const getPagination = (page, size) => {
+const getPagination = (pageIndex, size) => {
+  const page = +pageIndex - 1;
   const limit = size ? size : 3;
   const offset = page ? page * limit : 0;
   return { limit, offset };
