8af9c1b43e0fa1a42e4303764ad6bae3ba3499a7
ChovyDoinb <binhdinhqt137@gmail.com>
2021-11-17 10:20:54 +0700

    Edit: set default countInstock = 100

--- src/model/product.js
+++ src/model/product.js
@@ -13,7 +13,7 @@ const ProductSchema = new mongoose.Schema(
     size: { type: Array },
     color: { type: Array },
     price: { type: Number, required: true },
-    countInStock: { type: Number, default: 0 },
+    countInStock: { type: Number, default: 100 },
     countRating: { type: Number, default: 1 },
     rating: { type: String, default: "5.0" },
     popular: { type: String, default: randomPopular },
